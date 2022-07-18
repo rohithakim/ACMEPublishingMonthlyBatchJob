0f9199b2e5fa2a4960a8a7376a45c96f104b14a8
rohithakim <rohithakim@gmail.com>
2022-07-19 01:14:28 +1000

    Add files via upload

---
+++ Publication_MonthlyBatchJob/Utilites/CallWebApi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Publication_MonthlyBatchJob.Entities;
+
+namespace Publication_MonthlyBatchJob.Utilites
+{
+    public class CallWebApi
+    {
+ 
+        public void submitOrder(string webUrl, Customer cust, Magzine mag)
+        {
+            using (var client = new HttpClient())
+            {
+                //client.BaseAddress = new Uri(webUrl);
+                //client.DefaultRequestHeaders.Accept.Clear();
+                //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                ////GET Method
+                //HttpResponseMessage response = await client.GetAsync("api/SubGenerateOrder/customer");
+                //if (response.IsSuccessStatusCode)
+                //{
+                   
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Internal server Error");
+                //}
+            }
+        }
+    }
+}
---
+++ Publication_MonthlyBatchJob/Utilites/LogWriter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace Publication_MonthlyBatchJob.Utilites
+{
+    public class LogWriter
+    {
+        private string m_exePath = string.Empty;
+ 
+        public void LogWrite(string logMessage)
+        {
+            m_exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            try
+            {
+                using (StreamWriter w = File.AppendText(m_exePath + "\\" + "log.txt"))
+                {
+                    Log(logMessage, w);
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        public void Log(string logMessage, TextWriter txtWriter)
+        {
+            try
+            {
+                txtWriter.Write("\r\nLog Entry : ");
+                txtWriter.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
+                    DateTime.Now.ToLongDateString());
+                txtWriter.WriteLine("  :");
+                txtWriter.WriteLine("  :{0}", logMessage);
+                txtWriter.WriteLine("-------------------------------");
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
