3ba98494903ddfa03449ccf3d3ae04ad0c862ec0
rohithakim <rohithakim@gmail.com>
2022-07-19 00:48:32 +1000

    Add files via upload

---
+++ Publication_MonthlyBatchJob/ProcessSubscriptions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Publication_MonthlyBatchJob.Entities;
+using Publication_MonthlyBatchJob.Utilites;
+
+namespace Publication_MonthlyBatchJob
+{
+    public class ProcessSubscriptions
+    {
+
+        public List<Subscription> subcriptions = new List<Subscription>();
+        public List<Magzine> Magzines = new List<Magzine>();
+        public List<Company> PrintCompanies = new List<Company>();
+
+        public void addSubscription(Customer customer, Magzine mag, int subscriptionDurationinMonths, int quantity)
+        {
+            var subscription = new Subscription();
+            subscription.Customer = customer;
+
+            var order = new Order();
+             
+            order.Customer_ID = customer.ID;
+            order.CompanyID = PrintCompanies.Find(x => x.Zipcode == customer.ZipCode).ID;
+            order.Order_StartDate = DateTime.Now.Date;
+            order.Order_EndDate = DateTime.Now.AddMonths(subscriptionDurationinMonths);
+            order.Status = "A";
+
+            int orderID = 1;
+            int PaymentID = 1;
+            if(subcriptions.Count >0)
+            {
+                orderID = (subcriptions.SelectMany(x => x.Orders)).Max(x => x.ID) + 1;
+                PaymentID = (subcriptions.Select(x=> x.Paymentdetails)).Max(x=> x.ID) +1;
+            }
+            order.ID = orderID;
+
+            var subscriptionPayment = new Payment(PaymentID, mag.Price,DateTime.Now.Date);
+            subscription.Paymentdetails = subscriptionPayment;
+
+            subscription.Customer = customer;
+            subscription.Orders.Add(order);
+
+            var orderDetails = new OrderDetails();
+
+            orderDetails.MagzineID = mag.ID;
+            orderDetails.OrderID = orderID;
+            orderDetails.OrderDate = DateTime.Now.Date;
+            orderDetails.Quantity = quantity;
+
+            subscription.OrderDetails.Add(orderDetails);
+
+            subcriptions.Add(subscription);
+
+
+        }
+
+        public List<Order> getActiveSubsciptions()
+        {
+            List<Order> activeOrders = (subcriptions.SelectMany(x => x.Orders).ToList()).FindAll(x=> x.Status == "A");
+
+            return activeOrders;
+        }
+
+        public void sendMagzinesToCustomers(Order order)
+        {
+            Customer cust = subcriptions.Select(x => x.Customer).First(x => x.ID == order.Customer_ID);
+            OrderDetails orderDet = (subcriptions.SelectMany(x => x.OrderDetails).ToList()).Find(x => x.OrderID == order.ID);
+            Magzine mag = Magzines.Find(x => x.ID == orderDet.MagzineID);
+            Company company = PrintCompanies.Find(x => x.ID == order.CompanyID);
+
+            CallWebApi webApi = new CallWebApi();
+            webApi.submitOrder(company.WebApiUrl, cust, mag);
+        }
+    }
+}
---
+++ Publication_MonthlyBatchJob/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Publication_MonthlyBatchJob.Entities;
+using Publication_MonthlyBatchJob.Utilites;
+
+namespace Publication_MonthlyBatchJob
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+           
+            ProcessSubscriptions subcriptions = new ProcessSubscriptions();
+
+            #region Sample Datasetup
+            var sportILLustrated = new Magzine(1,"SportIllustrated",12);
+            var ReaderDigest = new Magzine(2, "ReaderDigest", 10);
+            var NationalGeographic = new Magzine(3, "NationalGeographic", 15);
+
+            subcriptions.Magzines.Add(sportILLustrated);
+            subcriptions.Magzines.Add(ReaderDigest);
+            subcriptions.Magzines.Add(NationalGeographic);
+
+            var vicPublishingHouse = new Company(1, "Vic Publishing House", "VIC", "Melbourne", "3000","http://companyurl/submitOrder/Customer/Magzine/");
+            var NSWPrintingPress = new Company(2, "NSW Printing Press", "NSW", "Sydney", "2000", "http://companyurl/submitOrder/Customer/Magzine/");
+            var QueenslandPublishing = new Company(3, "Queensland Publishing", "QLD", "Brisbane", "4000", "http://companyurl/submitOrder/Customer/Magzine/");
+
+            subcriptions.PrintCompanies.Add(vicPublishingHouse);
+            subcriptions.PrintCompanies.Add(NSWPrintingPress);
+            subcriptions.PrintCompanies.Add(QueenslandPublishing);
+
+            var VicCustomer = new Customer(1,"Jason","Holder","01241234","abc@gmail.com","VIC","Melbourne","Hamilton","3000");
+            var NSWCustomer = new Customer(2, "John", "Hann", "01241284", "xyz@gmail.com", "NSW", "Sydney", "Middleton", "2000");
+            var QLDCustomer = new Customer(3, "Mary", "Spicks", "022241234", "pqr@gmaail.com", "QLD", "Brisbane", "Living CR", "4000");
+
+            subcriptions.addSubscription(VicCustomer, sportILLustrated, 12, 1);
+            subcriptions.addSubscription(NSWCustomer, ReaderDigest, 6, 1);
+            subcriptions.addSubscription(QLDCustomer, NationalGeographic, 12, 1);
+
+            #endregion
+
+            try
+            {
+                List<Order> activeOrders = subcriptions.getActiveSubsciptions();
+
+                foreach(Order order in activeOrders)
+                {
+                    subcriptions.sendMagzinesToCustomers(order);
+                }
+
+            }
+            catch(Exception ex)
+            {
+                LogWriter log = new LogWriter();
+                log.LogWrite("Error: "  + ex.Message);
+            }
+
+        }
+    }
+}
